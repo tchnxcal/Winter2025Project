889d66e147d8e01817650d08bca485b9ebdd35f0
tchnxcal <tchnxcal@gmail.com>
2025-04-30 23:13:49 -0400

    Methods (CombatInterface)
    Methods added

--- src/main/java/org/example/CombatInterface.java
+++ src/main/java/org/example/CombatInterface.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public interface CombatInterface {
+    void performBasicAttack(AbstractUnit target);
+    void takeTurn();
+    void receiveDamage(int amount);
+    boolean isAlive();
 }
